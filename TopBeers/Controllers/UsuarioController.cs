ec0aca7606e4087136b2b55a578a7aba932f97bc
Bruno Costa <br9uno@gmail.com>
2018-08-15 17:45:44 -0300

    Transformando em asp.net

---
+++ TopBeers/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TopBeers.Models;
+
+namespace TopBeers.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult ValidarLogin(UsuarioModel usuario)
+        //{
+        //    if (login)
+        //    {
+        //        return RedirectToAction("Index", "Home");
+        //    } else
+        //    {
+        //        return RedirectToActio("Login", "Home");
+        //    }
+        //}
+
+        public IActionResult ValidarLogin(UsuarioModel usuario)
+        {
+            var user = usuario;
+
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ TopBeers/Models/UsuarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TopBeers.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime EDITAR_NAO_E_SENHA { get; set; }
+
+    }
+
+    //public bool ValidarLogin()
+    //{
+    //    string sql = $"SELECT ID, NOME, DATA_NASCIMENTO FROM USUARIO WHERE EMAIL'{Email}' AND SENHA'{Senha}'";
+    //    DAL objDAL = new DAL();
+    //    DataTable dt = objDAL.RetDataTable(sql);
+
+    //    if(dt != null)
+    //    {
+    //        if (dt.Rows.Count == 1)
+    //        {
+    //            return true;
+    //        }
+
+    //        return false;
+    //    }
+    //}
+}
